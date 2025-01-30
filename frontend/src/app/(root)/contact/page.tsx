845e1b208201af9bea294a2d41a16c68f45b7173
Shafayet <11shafayet@gmail.com>
2025-01-30 09:50:55 +0600

    updated navbar, about us page and contact page

--- frontend/src/app/(root)/contact/page.tsx
+++ frontend/src/app/(root)/contact/page.tsx
@@ -1,16 +1,21 @@
-import Form from "@/components/Form";
-import Social from "@/components/Social";
-
+import Form from '@/components/Form';
+import Social from '@/components/Social';
 
 const Contact = () => {
   return (
     <div className="xl:pl-[270px]">
       <div className="container px-4 mx-auto max-w-3xl py-12">
         <h2 className="text-3xl sm:text-[40px] mb-4">{`Let's Talk`}</h2>
-        <p className="font-extralight text-gray-400 mb-8">
+        <p className="text-lg text-gray-400 mb-8">
           {` If you'd like to talk about a potential project or just say hi, send
           me a message or email me at`}{' '}
-          <span className="text-white font-medium">enayetflweb@gmail.com</span>!
+          <a
+            href="mailto:enayetflweb@gmail.com"
+            className="text-white font-medium  underline"
+          >
+            enayetflweb@gmail.com
+          </a>
+          !
         </p>
         <Form />
         <Social center={true} marginTop={true} />
