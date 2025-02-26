50cae283f70e44582194b7c9caf355e0063a27de
Shafayet <11shafayet@gmail.com>
2025-02-26 20:35:49 +0600

    updated

--- frontend/src/components/Work.tsx
+++ frontend/src/components/Work.tsx
@@ -3,27 +3,9 @@
 import { useEffect, useState } from 'react';
 import Button from './Button';
 import FeaturedCard from './FeaturedCard';
+import { staticWorks } from '../../public/datas/data';
 
 const Work = () => {
-  const [projects, setProjects] = useState([]);
-
-  // Fetch all projects
-  const fetchProjects = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}project/get-all-projects`
-      );
-      const data = await response.json();
-      setProjects(data.data || []);
-    } catch (error) {
-      console.error('Error fetching projects:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProjects();
-  }, []);
-
   const buttonData = {
     link: '/works',
     text: 'View All Work',
@@ -33,7 +15,7 @@ const Work = () => {
     <div className="container px-4 mx-auto py-20">
       <h2 className="text-3xl font-medium">Full Stack Works</h2>
       <div className="grid grid-cols-3 gap-4 gap-y-12 lg:gap-20 py-12">
-        {projects?.slice(0, 3).map((item, i) => {
+        {staticWorks.map((item, i) => {
           return (
             <div className="col-span-3 md:col-span-1" key={i}>
               <FeaturedCard workData={item} />
